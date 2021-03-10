ab9120e50369214c42679670b255541ce8284a88
Philip Bergqvist <philip@pqvst.com>
2021-03-10 16:04:52 +0800

    WIP

--- sparklines.js
+++ sparklines.js
@@ -1,5 +1,6 @@
 /*
-Copyright (c) 2014 Reece Selwood
+Portions Copyright (c) 2014-2020 Philip Bergqvist
+Portions Copyright (c) 2014 Reece Selwood
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -19,10 +20,22 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-window.onload = function() { 
-  function makeChart(values, height, parent, filled) {
-    var width = 80;
-    height = height - 2;
+
+function sparklines(selector, opts) {
+  opts = opts || {};
+
+  function makeChart(values, parent) {
+    parent.innerHTML = '';
+
+    var width, height;
+    if (opts.inline) {
+      width = opts.width || parent.offsetWidth || 50;
+      height = opts.height || parent.offsetHeight || getDefaultFontSize(parent);
+    } else {
+      width = 100; //opts.width || parent.offsetWidth || 50;
+      height = 100; //opts.height || parent.offsetHeight || 12;
+    }
+
     var max = Math.max.apply(null, values);
     var min = Math.min.apply(null, values);
 
@@ -32,11 +45,16 @@ window.onload = function() {
     }
 
     var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('width', width);
-    svg.setAttribute('height', height);
     svg.setAttribute('transform', 'translate(0,1)');
+    if (opts.inline) {
+      svg.setAttribute('width', width);
+      svg.setAttribute('height', height);
+    } else {
+      svg.setAttribute('viewBox', '0,0,'+width+','+height);
+      svg.setAttribute('preserveAspectRatio', 'none');
+    }
 
-    var offset = Math.round(width/(values.length - 1));
+    var offset = width / (values.length - 1);
     var path = 'M0 ' + c(values[0]).toFixed(2);
     for (var i = 0; i < values.length; i++) {
       path += ' L ' + (i * offset) + ' ' + (c(values[i]).toFixed(2));
@@ -45,9 +63,10 @@ window.onload = function() {
     var pathElm = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     pathElm.setAttribute('d', path);
     pathElm.setAttribute('fill', 'none');
+    pathElm.setAttribute('vector-effect', 'non-scaling-stroke');
     svg.appendChild(pathElm);
 
-    if (filled) {
+    if (opts.filled) {
       path += ' V ' + height;
       path += ' L 0 ' + height + ' Z';
       var e = document.createElementNS('http://www.w3.org/2000/svg', 'path');
@@ -67,20 +86,17 @@ window.onload = function() {
 
     who.appendChild(document.createTextNode('M'));
     pa.appendChild(who);
-    var fs= [who.offsetWidth, who.offsetHeight];
+    var lineHeight = who.offsetHeight;
     pa.removeChild(who);
-    return fs;
- }
+    return lineHeight;
+  }
 
-  var elms = document.querySelectorAll('.sparkline');
+  var elms = document.querySelectorAll(selector);
   for (var i = 0; i < elms.length; i++) {
     var e = elms[i];
-    var values = elms[i].dataset.values.split(',').map(function(d) { return parseFloat(d); });
-    var height = getDefaultFontSize(e)[1];
-    if (e.classList.contains('sparkline-filled')) {
-      makeChart(values, height, e, true);
-    } else {
-      makeChart(values, height, e, false);
+    if (e.dataset.values) {
+      var values = e.dataset.values.split(',').map(function(d) { return parseFloat(d); });
+      makeChart(values, e);
     }
   }
 }
